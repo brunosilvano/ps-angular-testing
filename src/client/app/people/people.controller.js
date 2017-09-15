a4164b71d8408f116abc07bb176fbe78e878b680
Bruno Silvano <bruno_705@hotmail.com>
2017-09-15 14:41:23 -0300

    people state setup

---
+++ src/client/app/people/people.controller.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+  
+    angular
+      .module('app.people')
+      .controller('PeopleController', PeopleController);
+  
+    PeopleController.$inject = ['$q', 'dataservice', 'logger'];
+    /* @ngInject */
+    function PeopleController($q, dataservice, logger) {
+        var vm = this;
+        
+    }
+  })();
+  
